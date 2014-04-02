e35bb27d900580c82395e613a8a74d5fa89897f1
AaronPlave <aplave@wesleyan.edu>
2014-04-02 22:23:32 +0200

    Added media player to activity_main

--- Senuti/src/com/example/senuti/MainActivity.java
+++ Senuti/src/com/example/senuti/MainActivity.java
@@ -1,23 +1,50 @@
 package com.example.senuti;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+implements OnPreparedListener {
 
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//Bind button click to playMP3()
+		
+		Button button = (Button) findViewById(R.id.btnPlay);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				playMP3();
+			}
+		});
+	}
+
+	private void playMP3() {
+		try {
+			MediaPlayer mp =
+			  MediaPlayer.create(this,
+					R.sounds.sandstorm);
+			// MediaPlayer mp = new MediaPlayer();
+			// mp.setDataSource(Environment.getExternalStorageDirectory().getAbsolutePath()+"/serenity.mp3");
+			mp.setOnPreparedListener(this);
+			mp.prepare();
+		} catch (Exception e) {
+		}
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onPrepared(MediaPlayer mp) {
+		mp.start();
 	}
 
 }
