ee74c8c3a3f4d8bfabd9bca46137415559b4c4a1
AaronPlave <aplave@wesleyan.edu>
2014-04-05 22:05:06 +0200

    Reverse now reads the bytes starting from the correct position. Playing and pausing works. When at the beginning or end of the song, the player pauses until a valid option is chosen for playback or a signal is sent to terminate the player (not tested yet).

--- Senuti/src/com/example/senuti/MainActivity.java
+++ Senuti/src/com/example/senuti/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity implements OnPreparedListener {
 	ArrayList<MediaPlayer> mediaPlayers;
 	ArrayList<Button> buttons;
 	double sliderval;
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,49 +57,49 @@ public class MainActivity extends Activity implements OnPreparedListener {
 				atp.pause();
 			}
 		});
-		
-		//Bind the pitch slider for AT
+
+		// Bind the pitch slider for AT
 		SeekBar pitchSlider = (SeekBar) findViewById(R.id.pitchSlider);
-		
-        // create a listener for the slider bar;
-        OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
-          public void onStopTrackingTouch(SeekBar seekBar) { }
-          public void onStartTrackingTouch(SeekBar seekBar) { }
-          public void onProgressChanged(SeekBar seekBar, 
-                                          int progress,
-                                           boolean fromUser) {
-              if(fromUser) {
-            	  Log.d("TAG_ACTIVITY",Integer.toString(progress));
-            	  sliderval = progress / (double)seekBar.getMax();
-            	  Log.d("TAG_ACTIVITY",Double.toString(sliderval));
-            	  atp.setPitch(sliderval);
-            	  
-              }
-           }
-        };
 
-        // set the listener on the slider
-        pitchSlider.setOnSeekBarChangeListener(listener);
+		// create a listener for the slider bar;
+		OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
 
-		
-		
-		//Bind the reverse toggle for AT
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				if (fromUser) {
+					Log.d("TAG_ACTIVITY", Integer.toString(progress));
+					sliderval = progress / (double) seekBar.getMax();
+					Log.d("TAG_ACTIVITY", Double.toString(sliderval));
+					atp.setPitch(sliderval);
+
+				}
+			}
+		};
+
+		// set the listener on the slider
+		pitchSlider.setOnSeekBarChangeListener(listener);
+
+		// Bind the reverse toggle for AT
 		Switch atReverse = (Switch) findViewById(R.id.toggleReverse);
 		atReverse.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View view) {
-			    // Is the toggle on?
-			    boolean on = ((Switch) view).isChecked();
-			    
-			    if (on) {
-			        atp.setReverse(true);
-			    } else {
-			        atp.setReverse(false);
-			    }
+				// Is the toggle on?
+				boolean on = ((Switch) view).isChecked();
+
+				if (on) {
+					atp.setReverse(true);
+				} else {
+					atp.setReverse(false);
+				}
 			}
 		});
-		
 
 		// Bind backtrack AT file
 		Button atBack = (Button) findViewById(R.id.btnAudioTrackBack);
@@ -224,32 +224,34 @@ public class MainActivity extends Activity implements OnPreparedListener {
 		AudioThread audioThread;
 
 		public void play() {
-			if (audioThread == null){
-				Log.d("TAG_ACTIVITY","PLAY AUDIOTHREAD");
-				audioThread = new AudioThread();    
+			if (audioThread == null) {
+				Log.d("TAG_ACTIVITY", "PLAY AUDIOTHREAD");
+				audioThread = new AudioThread();
 				audioThread.setPriority(Thread.MAX_PRIORITY);
 				audioThread.start();
+			} else if (audioThread.PAUSED) {
+				// case where song is at beginning or end
+				audioThread.unPause();
+			} else {
+				Log.d("TAG_ACTIVITY",
+						"Play call disregarded, already playing/paused");
 			}
-			else {
-				Log.d("TAG_ACTIVITY","Play call disregarded, already playing/paused");
-			}
-			
 		}
 
 		public void pause() {
-			if (!(audioThread.isPaused())){
+			if (!(audioThread.isPaused())) {
 				audioThread.pause();
 			} else {
 				audioThread.unPause();
 			}
-			
+
 		}
-		
+
 		public void setPitch(double p) {
 			audioThread.setPitchOffset(p);
 		}
-		
-		public void setReverse(boolean r){
+
+		public void setReverse(boolean r) {
 			audioThread.setReverse(r);
 		}
 
@@ -286,41 +288,48 @@ public class MainActivity extends Activity implements OnPreparedListener {
 			boolean STOPPED = true;
 			int PITCHOFFSET = 0;
 			boolean CLEAR = false;
-			
+			int position = 0;
+			boolean MAX_POSITION = false;
+
 			int count = 16; // how many bytes to be read at a time
-			//TODO: maybe read in 16 bytes and then split up into 4 byte sections. 
-			//The more you read in at a time backwards, the worse it sounds, so try to reduce this.
-			//However, with a low count like 4, the OS can't really handle higher playback rates because it
-			//can't keep up with all of those reads. Is the solution to read 16 and reverse the frames?
-		
-			
-			
+			// TODO: maybe read in 16 bytes and then split up into 4 byte
+			// sections.
+			// The more you read in at a time backwards, the worse it sounds, so
+			// try to reduce this.
+			// However, with a low count like 4, the OS can't really handle
+			// higher playback rates because it
+			// can't keep up with all of those reads. Is the solution to read 16
+			// and reverse the frames?
+
 			private byte[] newArray;
 
 			@Override
 			public void run() {
-				Log.d("TAG_ACTIVITY","PLAYING");
+				Log.d("TAG_ACTIVITY", "PLAYING");
 				play();
 			}
-			
+
 			double MAX_OFFSET = 48000.0;
-			
-			public void setPitchOffset(double p){
-				if (p == 0.5) PITCHOFFSET = 0;
-				else if (p < 0.5) PITCHOFFSET = (int) ((0.5-p)*-MAX_OFFSET);
-				else PITCHOFFSET = (int) ((p-0.5)*MAX_OFFSET);
-				Log.d("TAG_ACTIVITY",Integer.toString(PITCHOFFSET)+" PO");
+
+			public void setPitchOffset(double p) {
+				if (p == 0.5)
+					PITCHOFFSET = 0;
+				else if (p < 0.5)
+					PITCHOFFSET = (int) ((0.5 - p) * -MAX_OFFSET);
+				else
+					PITCHOFFSET = (int) ((p - 0.5) * MAX_OFFSET);
+				Log.d("TAG_ACTIVITY", Integer.toString(PITCHOFFSET) + " PO");
 			}
-			
-			public void setReverse(boolean r){
+
+			public void setReverse(boolean r) {
 				REVERSE = r;
 			}
-			
+
 			public void unPause() {
 				PAUSED = false;
 				PLAYING = true;
 			}
-			
+
 			public boolean isPaused() {
 				return PAUSED;
 			}
@@ -333,26 +342,29 @@ public class MainActivity extends Activity implements OnPreparedListener {
 			public void clear() {
 				CLEAR = true;
 			}
-			
-			//This function reverses the frames in a byte array, assumes frames are 4 bytes long
-			public void reverseFrames (byte[] audio){
-				for (int i = 0; i < (Math.log(count)/Math.log(2)-2); i++){
-					//the log calculations is to calculate how many swaps are needed in a count sized array
-					for (int j = 0; j < 4; j++){
-						//grab jth byte in frame A
-						byte temp = audio[0+(i*4)+j];
-						
-						//set jth byte in frame B to jth byte in frame A
-						audio[0+(i*4)+j] = audio[audio.length-(i*4)-4+j];
-						
-						//reverse of above
-						audio[audio.length-(i*4)-4+j] = temp;
-					}	
+
+			// This function reverses the frames in a byte array, assumes frames
+			// are 4 bytes long
+			public void reverseFrames(byte[] audio) {
+				for (int i = 0; i < (Math.log(count) / Math.log(2) - 2); i++) {
+					// the log calculations is to calculate how many swaps are
+					// needed in a count sized array
+					for (int j = 0; j < 4; j++) {
+						// grab jth byte in frame A
+						byte temp = audio[0 + (i * 4) + j];
+
+						// set jth byte in frame B to jth byte in frame A
+						audio[0 + (i * 4) + j] = audio[audio.length - (i * 4)
+								- 4 + j];
+
+						// reverse of above
+						audio[audio.length - (i * 4) - 4 + j] = temp;
+					}
 				}
 			}
-			
-			//Currently unused really..
-			public void clipAudio(byte[] audio){
+
+			// Currently unused really..
+			public void clipAudio(byte[] audio) {
 				for (int i = 0; i < audio.length; i++) {
 					float data = audio[i];
 					// btw can mix samples by just adding them together
@@ -361,11 +373,11 @@ public class MainActivity extends Activity implements OnPreparedListener {
 					// float mixed = samplef1;
 					// mixed *= 0.8;
 					// hard clipping
-//					 if (data > 1.0f)
-//						 data = 1.0f;
-//					//
-//					 if (data < -1.0f)
-//						 data = -1.0f;
+					// if (data > 1.0f)
+					// data = 1.0f;
+					// //
+					// if (data < -1.0f)
+					// data = -1.0f;
 
 					byte output = (byte) (data);
 					audio[i] = output;
@@ -373,13 +385,12 @@ public class MainActivity extends Activity implements OnPreparedListener {
 			}
 
 			public void play() {
-				
+
 				if (at == null) {
 					Log.d("TAG_ACTIVITY", "audio track is not initialised ");
 					return;
 				}
-				Log.d("TAG_ACTIVITY","IN PLAY");
-				
+				Log.d("TAG_ACTIVITY", "IN PLAY");
 
 				// Reading the file..
 				InputStream in1 = getResources().openRawResource(
@@ -395,16 +406,8 @@ public class MainActivity extends Activity implements OnPreparedListener {
 
 				byte[] output = new byte[music1.length];
 
-				int bytesread = count * 2, ret = 0;
-
-				// int playbackRate = (int) (Math.pow(2.0, (1.0 / 12.0)) *
-				// 44100);
-
-				// at.setPlaybackRate(playbackRate);
-
 				// TO MIX--
-				
-				
+
 				for (int i = 44; i < output.length; i++) {
 					float samplef1 = music1[i];
 					// btw can mix samples by just adding them together
@@ -424,15 +427,35 @@ public class MainActivity extends Activity implements OnPreparedListener {
 				}
 
 				at.play();
-				Log.d("TAG_ACTIVITY","2");
-				while (bytesread < output.length) {
+				Log.d("TAG_ACTIVITY", "2");
+
+				boolean continuePlaying = true;
+				while (continuePlaying) {
+
+					// if (position > output.length){
+					// //reached the end of the song,
+					// MAX_POSITION = true;
+					// boolean wait = true;
+					// while (wait){
+					// if (REVERSE && )
+					// }
+					//
+					// }
+					// else if (position <= 0){
+					//
+					// }
+					if (!(position <= output.length && position >= 0)) {
+						// reached end/beginning of song
+						PAUSED = true;
+					}
+
 					// now we're playing, should do necessary checks for pause,
 					// stop, rewind, modify, etc.
 
 					// case where we want to kill the song completely and stop
 					// the player
 					if (CLEAR) {
-						Log.d("TAG_ACTIVITY","CLEAR");
+						Log.d("TAG_ACTIVITY", "CLEAR");
 						at.stop();
 						at.release();
 						return;
@@ -440,40 +463,53 @@ public class MainActivity extends Activity implements OnPreparedListener {
 
 					if (PAUSED) {
 						at.pause();
-						Log.d("TAG_ACTIVITY","PAUSED");
-						while (PAUSED) {
+						Log.d("TAG_ACTIVITY", "PAUSED");
+						boolean stayPaused = true;
+						while (stayPaused) {
 							if (CLEAR) {
 								at.stop();
 								at.release();
 								return;
+							} else if (!(PAUSED)) {
+								if (position <= 0 && !REVERSE) {
+									stayPaused = false;
+									position = 0;
+									Log.d("TAG_ACTIVITY","UNPAUSED, going forwards");
+								} else if (position >= output.length && REVERSE) {
+									stayPaused = false;
+									position = output.length - count;
+									Log.d("TAG_ACTIVITY","UNPAUSED, going backwards");
+								} else if (position >= 0 && position <= output.length - count){
+									stayPaused = false;
+								}
 							}
-							continue;
 						}
 						at.play();
 
 					}
-//					TODO: if possible, don't read whole song into memory
+					// TODO: if possible, don't read whole song into memory
 					// // Write the byte array to the track
-					at.setPlaybackRate(44100+PITCHOFFSET);
+					at.setPlaybackRate(44100 + PITCHOFFSET);
 					byte[] newArray;
+					newArray = Arrays.copyOfRange(output, position, position
+							+ count);
 					if (REVERSE) {
-						newArray = Arrays.copyOfRange(output, output.length
-								- bytesread, output.length - bytesread + count);
 						reverseFrames(newArray);
+						position -= count;
 					} else {
-						newArray = Arrays.copyOfRange(output, bytesread, bytesread+count);
+						position += count;
 					}
-					
+
 					clipAudio(newArray);
 
 					at.write(newArray, 0, count);
 					// playbackRate += 500;
-					bytesread += count;
 
 				}
 				at.stop();
 				// at.flush();
 				at.release();
+				PLAYING = false;
 			}
 		};
 
