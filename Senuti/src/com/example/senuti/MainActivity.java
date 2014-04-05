622cc75046c1746ee0e18d0f9fdfb8f7cd1aeffd
AaronPlave <aplave@wesleyan.edu>
2014-04-05 18:52:11 +0200

    Reverse and pitch UI elements bound and working properly. Discovered that reading in only 4 bytes at a time isn't very efficient and lags when reverse is pitch changed, so now reading in 16 (could be more) bytes and then reversing the frames in each array. This allows for much more efficient reverse pitch shifting.

--- Senuti/src/com/example/senuti/MainActivity.java
+++ Senuti/src/com/example/senuti/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.senuti;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -19,13 +18,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Switch;
 
 @SuppressLint("NewApi")
 public class MainActivity extends Activity implements OnPreparedListener {
 
 	ArrayList<MediaPlayer> mediaPlayers;
 	ArrayList<Button> buttons;
-
+	double sliderval;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +57,49 @@ public class MainActivity extends Activity implements OnPreparedListener {
 				atp.pause();
 			}
 		});
+		
+		//Bind the pitch slider for AT
+		SeekBar pitchSlider = (SeekBar) findViewById(R.id.pitchSlider);
+		
+        // create a listener for the slider bar;
+        OnSeekBarChangeListener listener = new OnSeekBarChangeListener() {
+          public void onStopTrackingTouch(SeekBar seekBar) { }
+          public void onStartTrackingTouch(SeekBar seekBar) { }
+          public void onProgressChanged(SeekBar seekBar, 
+                                          int progress,
+                                           boolean fromUser) {
+              if(fromUser) {
+            	  Log.d("TAG_ACTIVITY",Integer.toString(progress));
+            	  sliderval = progress / (double)seekBar.getMax();
+            	  Log.d("TAG_ACTIVITY",Double.toString(sliderval));
+            	  atp.setPitch(sliderval);
+            	  
+              }
+           }
+        };
+
+        // set the listener on the slider
+        pitchSlider.setOnSeekBarChangeListener(listener);
+
+		
+		
+		//Bind the reverse toggle for AT
+		Switch atReverse = (Switch) findViewById(R.id.toggleReverse);
+		atReverse.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+			    // Is the toggle on?
+			    boolean on = ((Switch) view).isChecked();
+			    
+			    if (on) {
+			        atp.setReverse(true);
+			    } else {
+			        atp.setReverse(false);
+			    }
+			}
+		});
+		
 
 		// Bind backtrack AT file
 		Button atBack = (Button) findViewById(R.id.btnAudioTrackBack);
@@ -178,9 +224,16 @@ public class MainActivity extends Activity implements OnPreparedListener {
 		AudioThread audioThread;
 
 		public void play() {
-			Log.d("TAG_ACTIVITY","PLAY AUDIOTHREAD");
-			audioThread = new AudioThread();    
-			audioThread.start();
+			if (audioThread == null){
+				Log.d("TAG_ACTIVITY","PLAY AUDIOTHREAD");
+				audioThread = new AudioThread();    
+				audioThread.setPriority(Thread.MAX_PRIORITY);
+				audioThread.start();
+			}
+			else {
+				Log.d("TAG_ACTIVITY","Play call disregarded, already playing/paused");
+			}
+			
 		}
 
 		public void pause() {
@@ -190,6 +243,14 @@ public class MainActivity extends Activity implements OnPreparedListener {
 				audioThread.unPause();
 			}
 			
+		}
+		
+		public void setPitch(double p) {
+			audioThread.setPitchOffset(p);
+		}
+		
+		public void setReverse(boolean r){
+			audioThread.setReverse(r);
 		}
 
 		public void rewind() {
@@ -221,16 +282,38 @@ public class MainActivity extends Activity implements OnPreparedListener {
 			boolean ALIVE = true;
 			boolean PLAYING = false;
 			boolean PAUSED = false;
-			boolean REVERSE = true;
+			boolean REVERSE = false;
 			boolean STOPPED = true;
 			int PITCHOFFSET = 0;
 			boolean CLEAR = false;
+			
+			int count = 16; // how many bytes to be read at a time
+			//TODO: maybe read in 16 bytes and then split up into 4 byte sections. 
+			//The more you read in at a time backwards, the worse it sounds, so try to reduce this.
+			//However, with a low count like 4, the OS can't really handle higher playback rates because it
+			//can't keep up with all of those reads. Is the solution to read 16 and reverse the frames?
+		
+			
+			
 			private byte[] newArray;
 
 			@Override
 			public void run() {
 				Log.d("TAG_ACTIVITY","PLAYING");
 				play();
+			}
+			
+			double MAX_OFFSET = 48000.0;
+			
+			public void setPitchOffset(double p){
+				if (p == 0.5) PITCHOFFSET = 0;
+				else if (p < 0.5) PITCHOFFSET = (int) ((0.5-p)*-MAX_OFFSET);
+				else PITCHOFFSET = (int) ((p-0.5)*MAX_OFFSET);
+				Log.d("TAG_ACTIVITY",Integer.toString(PITCHOFFSET)+" PO");
+			}
+			
+			public void setReverse(boolean r){
+				REVERSE = r;
 			}
 			
 			public void unPause() {
@@ -250,6 +333,44 @@ public class MainActivity extends Activity implements OnPreparedListener {
 			public void clear() {
 				CLEAR = true;
 			}
+			
+			//This function reverses the frames in a byte array, assumes frames are 4 bytes long
+			public void reverseFrames (byte[] audio){
+				for (int i = 0; i < (Math.log(count)/Math.log(2)-2); i++){
+					//the log calculations is to calculate how many swaps are needed in a count sized array
+					for (int j = 0; j < 4; j++){
+						//grab jth byte in frame A
+						byte temp = audio[0+(i*4)+j];
+						
+						//set jth byte in frame B to jth byte in frame A
+						audio[0+(i*4)+j] = audio[audio.length-(i*4)-4+j];
+						
+						//reverse of above
+						audio[audio.length-(i*4)-4+j] = temp;
+					}	
+				}
+			}
+			
+			//Currently unused really..
+			public void clipAudio(byte[] audio){
+				for (int i = 0; i < audio.length; i++) {
+					float data = audio[i];
+					// btw can mix samples by just adding them together
+
+					// reduce the volume a bit:
+					// float mixed = samplef1;
+					// mixed *= 0.8;
+					// hard clipping
+//					 if (data > 1.0f)
+//						 data = 1.0f;
+//					//
+//					 if (data < -1.0f)
+//						 data = -1.0f;
+
+					byte output = (byte) (data);
+					audio[i] = output;
+				}
+			}
 
 			public void play() {
 				
@@ -258,7 +379,7 @@ public class MainActivity extends Activity implements OnPreparedListener {
 					return;
 				}
 				Log.d("TAG_ACTIVITY","IN PLAY");
-				int count = 4; // how many bytes to be read at a time
+				
 
 				// Reading the file..
 				InputStream in1 = getResources().openRawResource(
@@ -282,6 +403,8 @@ public class MainActivity extends Activity implements OnPreparedListener {
 				// at.setPlaybackRate(playbackRate);
 
 				// TO MIX--
+				
+				
 				for (int i = 44; i < output.length; i++) {
 					float samplef1 = music1[i];
 					// btw can mix samples by just adding them together
@@ -329,16 +452,19 @@ public class MainActivity extends Activity implements OnPreparedListener {
 						at.play();
 
 					}
-
+//					TODO: if possible, don't read whole song into memory
 					// // Write the byte array to the track
-					 at.setPlaybackRate(44100);
+					at.setPlaybackRate(44100+PITCHOFFSET);
 					byte[] newArray;
-					if (!REVERSE) {
+					if (REVERSE) {
 						newArray = Arrays.copyOfRange(output, output.length
 								- bytesread, output.length - bytesread + count);
+						reverseFrames(newArray);
 					} else {
 						newArray = Arrays.copyOfRange(output, bytesread, bytesread+count);
 					}
+					
+					clipAudio(newArray);
 
 					at.write(newArray, 0, count);
 					// playbackRate += 500;
